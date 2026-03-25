9ef4ba438e55237337c121cffda6d97aa6620f8a
Ibrahim Bamba <ibamba366@gmail.com>
2026-03-25 20:16:37 +0000

    init: initialisation projet Spring Boot

---
+++ src/main/java/com/ibdev/assistant/AiSchoolAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.ibdev.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSchoolAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSchoolAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibdev/assistant/AiSchoolAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibdev.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSchoolAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
